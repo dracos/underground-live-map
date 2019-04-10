a9986cec3b9a78b6b0ab6bd2710382521e53b3cd
Web User <web@dracos.vm.bytemark.co.uk>
2019-04-10 12:52:23 +0100

    New stations, maybe.

--- bin/new-stations-from-api.py
+++ bin/new-stations-from-api.py
@@ -9,6 +9,12 @@ for t in types:
     j = json.load(urllib.urlopen('https://api.tfl.gov.uk/Line/%s/Route/Sequence/all' % t))
     for station in j['stations']:
         name = station['name'].replace(' Underground Station', ' Station')
+        if t == 'dlr' and 'DLR Station' not in name:
+            name += ' DLR Station'
+        if t == 'london-overground' and 'Rail Station' not in name:
+            name += ' Rail Station'
+        if t == 'tram' and 'Tram Stop' not in name:
+            name += ' Tram Stop'
         stations[name] = '%s,%s' % (station['lon'], station['lat'])
 
 json.dump(stations, open('stationsN.json', 'w'), sort_keys=True, indent=0)
